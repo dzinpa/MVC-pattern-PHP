0fd4a57103468a1570125c1d67543b0204d0cc75
Kristina <41165698+dzinpa@users.noreply.github.com>
2018-08-21 19:21:03 +0300

    Update Db.php

--- components/Db.php
+++ components/Db.php
@@ -2,14 +2,14 @@
 /**
  * Return object of class PDO
  * @params array from db_params.php
- * 
  */
 class DB 
 {
     public static function getConnection()
     {
         $paramsPath = ROOT . '/config/db_params.php';
-        $params = include ($paramsPath);//get array from db_params
+        $params = include ($paramsPath); //get array from db_params
+        
         //Create object PDO class
         $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
         $db = new PDO($dsn, $params['user'], $params['password']);
